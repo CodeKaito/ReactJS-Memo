6b04aefeb957b993d59e5ed0f013f635403b3ca2
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-05-16 13:37:50 +0200

    update

---
+++ src/TaskForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TaskForm = () => {
+  return (
+    <div>
+        <form>
+            <button>+</button>
+            <input type="text" />
+        </form>
+    </div>
+  )
+}
+
+export default TaskForm
