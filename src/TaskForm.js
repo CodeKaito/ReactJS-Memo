a7ffcc4d9867268002115681d83470f20bddbe41
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-05-16 15:25:00 +0200

    update

--- src/TaskForm.js
+++ src/TaskForm.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import {useState} from "react";
 
-const TaskForm = () => {
+export default function TaskForm({onAdd}) {
+  const [taskName,setTaskName] = useState('');
+  function handleSubmit(ev) {
+    ev.preventDefault();
+    onAdd(taskName);
+    setTaskName('');
+  }
   return (
-    <div>
-        <form>
-            <button>+</button>
-            <input type="text" />
-        </form>
-    </div>
-  )
+    <form onSubmit={handleSubmit}>
+      <button>+</button>
+      <input type="text"
+             value={taskName}
+             onChange={ev => setTaskName(ev.target.value)}
+             placeholder="Your next task..."/>
+    </form>
+  );
 }
-
-export default TaskForm
